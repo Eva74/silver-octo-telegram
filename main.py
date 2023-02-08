1b68019b5110901e1e0896b995db0b812a88b163
Eva74 <emsalas14@gmail.com>
2023-02-08 11:36:10 -0600

    Feature 1: adding odd/even functionality to utils

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from utils import find_avg
 from utils import find_median
 from utils import rev_list
 from utils import is_palindrome
+from utils import is_odd_or_even
 
 myList = [0, 2, 3, 4, 5, 6]
 print("The average of the list is: " + str(find_avg(myList)))
@@ -11,3 +12,6 @@ print("The list in reverse is: " + str(myList))
 
 myString = 'salas'
 print(is_palindrome(myString))
+
+print("Is the first element in the list even?")
+print(is_odd_or_even(myList[0]))
