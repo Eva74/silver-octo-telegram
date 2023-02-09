5aff3e77f3fe55f861d627a66c2761a6249e51b9
Eva74 <emsalas14@gmail.com>
2023-02-09 11:22:31 -0600

    Feature 1: adding unit test to odd/even functionality in utils

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from utils import find_median
 from utils import rev_list
 from utils import is_palindrome
 from utils import is_odd_or_even
+from utils import int_to_char
 
 myList = [0, 2, 3, 4, 5, 6]
 print("The average of the list is: " + str(find_avg(myList)))
@@ -15,3 +16,6 @@ print(is_palindrome(myString))
 
 print("Is the first element in the list even?")
 print(is_odd_or_even(myList[0]))
+
+num = 65
+print(int_to_char(num))
